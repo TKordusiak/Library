b84a889dfec0d92d40f68c7b5bd58b02651d7787
TKordusiak <tomasz_kordusiak@o2.pl>
2019-04-04 22:53:31 +0200

    data

--- Library/src/app/LibraryApp.java
+++ Library/src/app/LibraryApp.java
@@ -4,7 +4,7 @@ public class LibraryApp {
 
 	public static void main(String[] args) {
 		
-		final String APP_NAME = "Biblioteka v0.7";
+		final String APP_NAME = "Biblioteka v0.7 4.04";
 		System.out.println(APP_NAME);
 		LibraryControl libraryControl = new LibraryControl();
 		libraryControl.controlLoop();
